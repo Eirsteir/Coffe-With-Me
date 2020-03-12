c31f8654bd301b3a28800afd171e536069fc3e03
Eirik Steira <eirsteir@stud.ntnu.no>
2020-03-12 18:22:07 +0100

    Initial commit

---
+++ src/main/java/com/eirsteir/coffewithme/CoffeWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.eirsteir.coffewithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eirsteir/coffewithme/CoffeWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eirsteir.coffewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
