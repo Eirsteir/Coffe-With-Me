b9641556422e30573931295ce84a85c24fb59810
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-01 11:22:14 +0200

    Update UserControllerTest with recent changes

--- src/test/java/com/eirsteir/coffeewithme/web/UserControllerTest.java
+++ src/test/java/com/eirsteir/coffeewithme/web/UserControllerTest.java
@@ -1,6 +1,6 @@
 package com.eirsteir.coffeewithme.web;
 
-import com.eirsteir.coffeewithme.domain.user.User;
+import com.eirsteir.coffeewithme.dto.UserDto;
 import com.eirsteir.coffeewithme.service.UserService;
 import com.eirsteir.coffeewithme.web.v1.UserController;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,17 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.Collection;
+import java.util.Map;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -32,6 +32,7 @@ class UserControllerTest {
     public static final String USERNAME_ALEX = "alex";
     public static final String EMAIL_ALEX = "alex@email.com";
     public static final String PASSWORD_ALEX = "12345678";
+    public static final String NAME_ALEX = "Alex";
 
     @Autowired
     private MockMvc mockMvc;
@@ -39,93 +40,67 @@ class UserControllerTest {
     @MockBean
     private UserService userService;
 
-    private User user;
-    private List<User> allUsers = new ArrayList<>();
+    private UserDto userDto;
+    private OAuth2User principal;
 
     @BeforeEach
     public void setup() {
-        user = User.builder()
-                .id(1L)
+        principal = new OAuth2User() {
+            @Override
+            public Map<String, Object> getAttributes() {
+                return Map.of(
+                        "name", NAME_ALEX,
+                        "id", "123"
+                );
+            }
+
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return null;
+            }
+
+            @Override
+            public String getName() {
+                return NAME_ALEX;
+            }
+        };
+
+        userDto = UserDto.builder()
                 .username("alex")
                 .email("alex@email.com")
-                .password("12345678")
+                .name(NAME_ALEX)
                 .build();
 
-        User otherUser = User.builder()
-                .id(2L)
-                .username("adam")
-                .email("adam@email.com")
-                .password("12345678")
-                .build();
+        Mockito.when(userService.signUp(Mockito.any(UserDto.class)))
+                .thenReturn(userDto);
 
-        allUsers.add(user);
-        allUsers.add(otherUser);
-
-        given(userService.getAllUsers()).willReturn(allUsers);
-
-        given(userService.findById(1L)).willReturn(Optional.ofNullable(user));
-
-        Mockito.when(userService.saveUser(Mockito.any(NewUserForm.class)))
-                .thenReturn(user);
-
-        Mockito.when(userService.updateProfile(Mockito.any(User.class)))
-                .thenReturn(user);
+        Mockito.when(userService.updateProfile(Mockito.any(UserDto.class)))
+                .thenReturn(userDto);
     }
 
-    @Test
-    void testGetAllUsersReturnsJsonArrayWhenEmployeesGiven() throws Exception {
-
-        mockMvc.perform(get("/users")
-            .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content()
-                                   .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].username", is(user.getUsername())));
-    }
-
-    @Test
-    void testGetUserByIdReturnsUser() throws Exception {
-
-        mockMvc.perform(get("/users/{id}", 1)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-                .andExpect(content()
-                                   .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id", is(user.getId().intValue())))
-                .andExpect(jsonPath("$.username", is(user.getUsername())));
-    }
 
     @Test
     void testCreateUserReturnsCreatedUser() throws Exception {
-        NewUserForm newUserForm = NewUserForm.builder()
-                .username(USERNAME_ALEX)
-                .email(EMAIL_ALEX)
-                .verifyEmail(EMAIL_ALEX)
-                .password(PASSWORD_ALEX)
-                .verifyPassword(PASSWORD_ALEX)
-                .build();
 
         mockMvc.perform(post("/users")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(asJsonString(newUserForm)))
+                                .content(asJsonString(principal)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id", is(user.getId().intValue())))
-                .andExpect(jsonPath("$.username", is(user.getUsername())));
+                .andExpect(jsonPath("$.name", is(NAME_ALEX)));
     }
 
     @Test
-    void testUpdateUserReturnsUpdatedUser() throws Exception {
-       user.setUsername("updatedUsername");
+    void testUpdateProfileReturnsUpdatedUserDto() throws Exception {
+       userDto.setUsername(USERNAME_ALEX);
 
         mockMvc.perform(put("/users")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(asJsonString(user)))
+                                .content(asJsonString(userDto)))
                 .andExpect(status().isOk())
                 .andExpect(content()
                                    .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.id", is(user.getId().intValue())))
-                .andExpect(jsonPath("$.username", is("updatedUsername")));
+                .andExpect(jsonPath("$.email", is(EMAIL_ALEX)))
+                .andExpect(jsonPath("$.username", is(USERNAME_ALEX)));
     }
 
     private static String asJsonString(Object obj) {
