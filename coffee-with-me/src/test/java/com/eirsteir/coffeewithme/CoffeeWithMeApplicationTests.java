b43cdd15473d669a4ac84b9b88eba43f5ef8678b
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-02 19:55:09 +0200

    Initial

---
+++ coffee-with-me/src/test/java/com/eirsteir/coffeewithme/CoffeeWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eirsteir.coffeewithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
