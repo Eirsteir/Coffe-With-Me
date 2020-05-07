f7a8c624894310ca5b9514818dd05ac45fa69637
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-07 21:52:04 +0200

    Add friends list in UI

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -8,6 +8,36 @@ class Dashboard extends React.Component {
         super(props);
     }
 
+    componentDidMount() {
+        const token = window.localStorage.getItem("auth");
+        this.fetchFriends(token);
+    }
+
+    fetchFriends = token => {
+        fetch(`/api/user/friends`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token
+            }
+        })
+            .then(response => {
+                if (response.status === 400) {
+                    return this.toggleLoading();
+                }
+                return response.json();
+            })
+            .then(friends => {
+                this.toggleLoading();
+                if (friends) {
+                    return this.setState({ friends: friends})
+                }
+            })
+            .catch(err => {
+                this.toggleLoading();
+            });
+    };
+    
     render() {
         const { loadUser, user } = this.props;
 
@@ -18,6 +48,17 @@ class Dashboard extends React.Component {
         return (
             <div className="dashboard-container">
                 <h2>Welcome {user.name}</h2>
+                { this.state.friends && (
+                    <ul>
+                        {this.state.friends.map(friend => (
+                            <li key={friend.id}>
+                                <div>{friend.id}</div>
+                                <div>{friend.name}</div>
+                                { friend.username ? <div>{friend.username}</div> :  null}
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
         );
     }
