07ef3f4f8bf72c5667b29ffd6d4462eb4e32eb7d
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-15 15:05:29 +0200

    Update landing page styles

--- client/src/containers/LandingPage/LandingPage.js
+++ client/src/containers/LandingPage/LandingPage.js
@@ -4,71 +4,86 @@ import { Link } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-import Toolbar from "@material-ui/core/Toolbar";
+
+import "./LandingPage.css";
 
 const LandingPage = ({ onRouteChange }) => {
     return (
-        <Grid
-            container
-            justify="space-around"
-            direction="column"
-            spacing={8}
-            wrap="wrap"
-            id="main-container"
-        >
-            <Grid item>
-                <Typography
-                    variant="h2"
-                    style={{ color: "#fff", fontWeight: "bold" }}
-                    id="header"
-                >
-                    Coffee With Me
-                </Typography>
-            </Grid>
-
-            <Grid item>
-                <Typography
-                    variant="subtitle1"
-                    style={{ color: "#fff", fontWeight: 300 }}
-                    id="sub-header"
-                >
-                    Grab a warm cup of coffee with your friends
-                </Typography>
-            </Grid>
-
-            <Grid item>
-                <Button
-                    variant="outlined"
-                    style={{ backgroundColor: "#dd1173", padding: "20px 40px" }}
-                    id="register-now-btn"
-                >
-                    <Link
-                        to="/register"
-                        style={{
-                            textDecoration: "none",
-                            color: "#fff",
-                            textTransform: "capitalize",
-                            fontSize: "1rem",
-                            letterSpacing: 1
-                        }}
+        <div id="landing-page-container">
+            <Grid
+                container
+                spacing={1}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '70vh' }}
+            >
+                <Grid item>
+                    <Typography
+                        variant="h2"
+                        style={{ color: "#fff", fontWeight: "bold" }}
+                        id="header"
                     >
-                        Register
-                    </Link>
-                    <Link
-                        to="/login"
-                        style={{
-                            textDecoration: "none",
-                            color: "#fff",
-                            textTransform: "capitalize",
-                            fontSize: "1rem",
-                            letterSpacing: 1
-                        }}
+                        Coffee With Me
+                    </Typography>
+                </Grid>
+
+                <Grid item>
+                    <Typography
+                        variant="subtitle1"
+                        style={{ color: "#fff", fontWeight: 300 }}
+                        id="sub-header"
                     >
-                        Login
-                    </Link>
-                </Button>
+                        Grab a warm cup of coffee with your friends
+                    </Typography>
+                </Grid>
+
+                <Grid container
+                      spacing={2}
+                      direction="row"
+                      alignItems="center"
+                      justify="center"
+                      style={{ minHeight: '100%',marginTop: "1rem" }}
+                >
+                    <Grid item>
+                        <Button
+                            style={{ backgroundColor: "#dd1173", padding: "0.5rem 0", width: "8rem" }}
+                        >
+                            <Link
+                                to="/register"
+                                style={{
+                                    textDecoration: "none",
+                                    color: "#fff",
+                                    textTransform: "capitalize",
+                                    fontSize: "1rem",
+                                    letterSpacing: 1
+                                }}
+                            >
+                                Register
+                            </Link>
+                        </Button>
+                    </Grid>
+                    <Grid item>
+                        <Button
+                            style={{ backgroundColor: "#dd1173", padding: "0.5rem 0", width: "8rem"  }}
+                        >
+                            <Link
+                                to="/login"
+                                style={{
+                                    textDecoration: "none",
+                                    color: "#fff",
+                                    textTransform: "capitalize",
+                                    fontSize: "1rem",
+                                    letterSpacing: 1
+                                }}
+                            >
+                                Login
+                            </Link>
+                        </Button>
+                    </Grid>
+                </Grid>
             </Grid>
-        </Grid>
+        </div>
     );
 };
 
