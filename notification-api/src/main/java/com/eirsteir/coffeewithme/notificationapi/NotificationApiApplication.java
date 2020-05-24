5863f979b01a12cfe3c9e2467986822d4dcffd06
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-24 14:05:00 +0200

    Update Dockerfile in auth to expose 9100 and remove redis dependencies from notification-api

--- notification-api/src/main/java/com/eirsteir/coffeewithme/notificationapi/NotificationApiApplication.java
+++ notification-api/src/main/java/com/eirsteir/coffeewithme/notificationapi/NotificationApiApplication.java
@@ -2,10 +2,8 @@ package com.eirsteir.coffeewithme.notificationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.FlushMode;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@EnableRedisHttpSession(flushMode = FlushMode.IMMEDIATE)
+
 @SpringBootApplication
 public class NotificationApiApplication {
 
