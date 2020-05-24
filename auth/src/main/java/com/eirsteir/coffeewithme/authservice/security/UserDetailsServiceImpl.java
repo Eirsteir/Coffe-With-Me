e0e7ccb47be05f3fcb0ae875f3cfe2e85d9b7115
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-24 13:28:04 +0200

    Create auth service and add security config and jwt filter

---
+++ auth/src/main/java/com/eirsteir/coffeewithme/authservice/security/UserDetailsServiceImpl.java
@@ -0,0 +1,49 @@
+package com.eirsteir.coffeewithme.authservice.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO: 24.05.2020 Properly implement this
+
+        final List<AppUser> users = Arrays.asList(
+                new AppUser(1L, "omar", encoder.encode("12345"), "ROLE_USER"),
+                new AppUser(2L, "admin", encoder.encode("12345"), "ROLE_ADMIN")
+        );
+
+        for (AppUser appUser : users) {
+            if (appUser.getUsername().equals(username)) {
+                List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                        .commaSeparatedStringToAuthorityList(appUser.getRole());
+                return new User(appUser.getUsername(), appUser.getPassword(), grantedAuthorities);
+            }
+        }
+
+        throw new UsernameNotFoundException("Username: " + username + " not found");
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private static class AppUser {
+        private Long id;
+        private String username, password;
+        private String role;
+    }
+}
