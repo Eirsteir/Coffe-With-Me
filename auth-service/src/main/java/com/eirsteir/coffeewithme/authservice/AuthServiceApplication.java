29e905d941e3bc59e330ac7972649007d7e06431
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-25 17:28:18 +0200

    Rename auth to auth-service and add eureka client

--- auth-service/src/main/java/com/eirsteir/coffeewithme/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/eirsteir/coffeewithme/authservice/AuthServiceApplication.java
@@ -2,12 +2,14 @@ package com.eirsteir.coffeewithme.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class AuthApplication {
+@EnableEurekaClient
+public class AuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+        SpringApplication.run(AuthServiceApplication.class, args);
     }
 
 }
