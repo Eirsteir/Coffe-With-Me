18391e6138b2ef69ab0e0e895dd0f98b54b0b0ff
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-26 15:42:59 +0200

    Fix mysql config

--- social-service/src/main/java/com/eirsteir/coffeewithme/social/SocialServiceApplication.java
+++ social-service/src/main/java/com/eirsteir/coffeewithme/social/SocialServiceApplication.java
@@ -1,13 +1,9 @@
 package com.eirsteir.coffeewithme.social;
 
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.sql.SQLException;
 
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.eirsteir.coffeewithme.social.repository")
@@ -18,9 +14,4 @@ public class SocialServiceApplication {
         SpringApplication.run(SocialServiceApplication.class, args);
     }
 
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseServer() throws SQLException {
-        return Server.createTcpServer(
-                "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
-    }
 }
