15e420196738ff06733e9f16e095a99dbfe8b59e
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-26 16:00:11 +0200

    Fix routing to /auth/register

--- auth-service/src/main/java/com/eirsteir/coffeewithme/authservice/web/api/AuthController.java
+++ auth-service/src/main/java/com/eirsteir/coffeewithme/authservice/web/api/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -17,6 +18,7 @@ import java.util.Optional;
 
 @Slf4j
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired
