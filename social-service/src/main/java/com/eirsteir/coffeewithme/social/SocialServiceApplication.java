2711f25f34a713c89bee1da8eb17538f499bc750
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-29 17:30:07 +0200

    Imported controller advice from commons to social service

--- social-service/src/main/java/com/eirsteir/coffeewithme/social/SocialServiceApplication.java
+++ social-service/src/main/java/com/eirsteir/coffeewithme/social/SocialServiceApplication.java
@@ -3,10 +3,12 @@ package com.eirsteir.coffeewithme.social;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.eirsteir.coffeewithme.social.repository")
+@ComponentScan(basePackages = {"com.eirsteir.coffeewithme.commons.exception", "com.eirsteir.coffeewithme.social"})
 @SpringBootApplication
 public class SocialServiceApplication {
 
