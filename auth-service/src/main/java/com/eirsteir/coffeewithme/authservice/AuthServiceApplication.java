f4fb07b78795acc8de4554877c8f86b92c0ff6ad
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-29 17:33:49 +0200

    Imported controller advice from commons to remaining services

--- auth-service/src/main/java/com/eirsteir/coffeewithme/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/eirsteir/coffeewithme/authservice/AuthServiceApplication.java
@@ -3,10 +3,12 @@ package com.eirsteir.coffeewithme.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.eirsteir.coffeewithme.authservice.repository")
+@ComponentScan(basePackages = {"com.eirsteir.coffeewithme.commons.exception", "com.eirsteir.coffeewithme.authservice"})
 @SpringBootApplication
 public class AuthServiceApplication {
 
