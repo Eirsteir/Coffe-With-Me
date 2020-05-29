032cdaf2369cdfe27b03f87f27d4532073c9fe9e
Eirik Steira <eirsteir@stud.ntnu.no>
2020-05-29 17:47:50 +0200

    Update exception response and handling of invalid method argument

--- commons/src/main/java/com/eirsteir/coffeewithme/commons/exception/CWMResponseEntityExceptionHandler.java
+++ commons/src/main/java/com/eirsteir/coffeewithme/commons/exception/CWMResponseEntityExceptionHandler.java
@@ -1,19 +1,22 @@
 package com.eirsteir.coffeewithme.commons.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @Slf4j
 @ControllerAdvice
@@ -63,23 +66,27 @@ public class CWMResponseEntityExceptionHandler extends ResponseEntityExceptionHa
                                                                   HttpHeaders headers,
                                                                   HttpStatus status,
                                                                   WebRequest request) {
-        String errors = getErrorsFrom(ex);
+        List<String> errors = new ArrayList<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors())
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors())
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+
 
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(),
+                                                                    HttpStatus.BAD_REQUEST,
                                                                     "Validation failed",
-                                                                    errors,
-                                                                    status);
+                                                                    ex.getLocalizedMessage(),
+                                                                    errors);
         log.error("[x] Validation failed: {}", errors, ex);
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+
+        return handleExceptionInternal(ex,
+                                       exceptionResponse,
+                                       headers,
+                                       exceptionResponse.getStatus(),
+                                       request);
     }
 
-    private String getErrorsFrom(MethodArgumentNotValidException ex) {
-        return ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .filter(Objects::nonNull)
-                .map(m -> m.replace(".", ""))
-                .collect(Collectors.joining(", "));
-    }
 }
