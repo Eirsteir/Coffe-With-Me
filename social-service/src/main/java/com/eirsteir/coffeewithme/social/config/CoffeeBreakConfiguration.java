ec5f6fdbb61ec713ae2a008c405865d0a880c16c
Eirik Steira <eirsteir@stud.ntnu.no>
2020-10-25 17:47:28 +0100

    Fix coffee break service tests

--- social-service/src/main/java/com/eirsteir/coffeewithme/social/config/CoffeeBreakConfiguration.java
+++ social-service/src/main/java/com/eirsteir/coffeewithme/social/config/CoffeeBreakConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CoffeeBreakConfiguration {
 
+
     @Bean
     public CoffeeBreakService coffeeBreakService(DomainEventPublisher domainEventPublisher,
                                                  CoffeeBreakRepository coffeeBreakRepository,
