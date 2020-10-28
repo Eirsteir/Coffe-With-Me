3e669ea4383e3b2b5dd43a669353afd3800df35a
Eirik Steira <eirsteir@stud.ntnu.no>
2020-10-28 19:01:55 +0100

    Add same test config to UserControllerTest

--- social-service/src/test/java/com/eirsteir/coffeewithme/social/web/api/user/UserControllerTest.java
+++ social-service/src/test/java/com/eirsteir/coffeewithme/social/web/api/user/UserControllerTest.java
@@ -1,17 +1,15 @@
 package com.eirsteir.coffeewithme.social.web.api.user;
 
+import com.eirsteir.coffeewithme.social.config.ModelMapperConfig;
 import com.eirsteir.coffeewithme.social.security.SecurityConfig;
-import com.eirsteir.coffeewithme.social.web.api.friendship.FriendshipController;
-import org.junit.jupiter.api.extension.ExtendWith;
+import com.eirsteir.coffeewithme.testconfig.EventuateTestConfig;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ActiveProfiles;
 
-@Import({SecurityConfig.class})
-@TestPropertySource("classpath:exception.properties")
-@WebMvcTest(FriendshipController.class)
-@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@Import({SecurityConfig.class, ModelMapperConfig.class, EventuateTestConfig.class})
+@WebMvcTest(UserController.class)
 class UserControllerTest {
 
 
