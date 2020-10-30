bfb669fbfe1ccae98b8ee0ad4fc0911c0b45c1ab
Eirik Steira <eirsteir@stud.ntnu.no>
2020-10-30 18:11:44 +0100

    Add H2 to notification service tests

--- notification-service/src/test/java/com/eirsteir/coffeewithme/notification/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/eirsteir/coffeewithme/notification/NotificationServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.eirsteir.coffeewithme.notification;
 import com.eirsteir.coffeewithme.notification.config.EventuateTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = {NotificationServiceApplication.class, EventuateTestConfig.class})
 class NotificationServiceApplicationTests {
 
