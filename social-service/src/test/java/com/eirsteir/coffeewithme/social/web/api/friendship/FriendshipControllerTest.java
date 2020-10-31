e0e2a4925386b317cbd7fffda8e0b276d99665c1
Eirik Steira <eirsteir@stud.ntnu.no>
2020-10-31 12:41:17 +0100

    Fix mocks in friendship controller tests

--- social-service/src/test/java/com/eirsteir/coffeewithme/social/web/api/friendship/FriendshipControllerTest.java
+++ social-service/src/test/java/com/eirsteir/coffeewithme/social/web/api/friendship/FriendshipControllerTest.java
@@ -10,7 +10,9 @@ import com.eirsteir.coffeewithme.social.config.ModelMapperConfig;
 import com.eirsteir.coffeewithme.social.domain.friendship.FriendshipStatus;
 import com.eirsteir.coffeewithme.social.domain.user.User;
 import com.eirsteir.coffeewithme.social.dto.FriendshipDto;
+import com.eirsteir.coffeewithme.social.repository.*;
 import com.eirsteir.coffeewithme.social.security.SecurityConfig;
+import com.eirsteir.coffeewithme.social.service.coffeebreak.CoffeeBreakService;
 import com.eirsteir.coffeewithme.social.service.friendship.FriendshipService;
 import com.eirsteir.coffeewithme.social.service.user.UserService;
 import com.eirsteir.coffeewithme.social.web.request.FriendRequest;
@@ -21,7 +23,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -60,9 +64,41 @@ class FriendshipControllerTest {
 
   @MockBean private UserDetailsService userDetailsService;
 
-  @MockBean private FriendshipService friendshipService;
+  @Autowired private FriendshipService friendshipService;
 
-  @MockBean private UserService userService;
+  @Autowired private UserService userService;
+
+  @TestConfiguration
+  static class FriendshipControllerTestConfiguration {
+
+    @MockBean
+    private CoffeeBreakRepository coffeeBreakRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+    @MockBean
+    private CampusRepository campusRepository;
+    @MockBean
+    private FriendshipRepository friendshipRepository;
+    @MockBean
+    private UniversityRepository universityRepository;
+    @Bean
+    public FriendshipService friendshipService()
+    {
+      return Mockito.mock(FriendshipService.class);
+    }
+    @Bean
+    public UserService userService()
+    {
+      return Mockito.mock(UserService.class);
+    }
+    @Bean
+    public CoffeeBreakService coffeeBreakService()
+    {
+      return Mockito.mock(CoffeeBreakService.class);
+    }
+
+  }
 
   @Autowired private JwtConfig jwtConfig;
 
